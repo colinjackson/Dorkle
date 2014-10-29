58ffd40d27f70d5645dc9dec15193c48f6e2e252
Colin Jackson <colin.s.jackson90@gmail.com>
2014-10-29 17:41:08 -0400

    Add game methods (and specs) to round model

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,8 +1,57 @@
 class Round < ActiveRecord::Base
   validates_presence_of :game
 
-  belongs_to :game, inverse_of: :rounds
   belongs_to :player, class_name: "User", inverse_of: :rounds
+  belongs_to :game, inverse_of: :rounds
   has_many :answers, through: :game, source: :answers
   has_many :answer_matches, class_name: "RoundAnswerMatch", inverse_of: :round
+
+  def handle_guess(guess)
+    answer = answer_for_guess(guess)
+    self.answer_matches.create!(answer: answer) if answer
+  end
+
+  def answer_for_guess(guess)
+    self.answers
+      .includes(:matches)
+      .where(answer: guess, round_answer_matches: {id: nil})
+      .first
+  end
+
+  def answers_left
+    self.answers
+      .includes(:matches)
+      .where(round_answer_matches: {id: nil})
+      .count
+  end
+
+  def time_remaining
+    self.game.time_limit - (Time.now - self.created_at)
+  end
+
+  # SAVE the SQL version for reference/posterity:
+  #
+  # def answer_for_guess(guess)
+  #   self.answers.find_by_sql(<<-SQL).first
+  #   SELECT
+  #     answers.*
+  #   FROM
+  #     game_answers AS answers
+  #   INNER JOIN
+  #     games
+  #   AS games ON answers.game_id = games.id
+  #   LEFT OUTER JOIN (
+  #     SELECT
+  #       round_answer_matches.*
+  #     FROM
+  #       round_answer_matches
+  #     WHERE
+  #       round_answer_matches.round_id = #{self.id}
+  #   ) AS matches ON matches.answer_id = answers.id
+  #   WHERE
+  #     answers.answer = '#{guess}' AND
+  #     matches.id IS NULL
+  #   SQL
+  # end
+
 end
--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -21,4 +21,47 @@ RSpec.describe Round, :type => :model do
     it { should have_many(:answer_matches)}
   end
 
+  describe "methods" do
+    let(:round) { create(:round) }
+    before do
+      ("a".."c").each do |letter|
+        round.game.answers.create(answer: letter)
+      end
+    end
+
+    describe "checking guesses" do
+      it "returns the appropriate answer object for a correct guess" do
+        answer = round.answer_for_guess("a")
+        expect(answer).to be_a(GameAnswer)
+        expect(answer.answer).to eq("a")
+      end
+
+      it "creates an answer match object for a correct guess" do
+        answer = round.answers.first
+        expect(round.answer_matches).to receive(:create!).with(answer: answer)
+
+        round.handle_guess(answer.answer)
+      end
+
+      it "does not return an answer if it's already been matched" do
+        round.handle_guess("a")
+        expect(round.answer_for_guess("a")).to be_nil
+      end
+
+    end
+
+    describe "reporting gamestate" do
+      it "reports the remaining number of answers to be found" do
+        expect(round.answers_left).to be(3)
+        round.handle_guess("a")
+        expect(round.answers_left).to be(2)
+      end
+
+      it "reports the remaining time left in the game" do
+        allow(round).to receive(:created_at).and_return(2.minutes.ago)
+        expect(round.time_remaining).to be_within(0.5).of(0)
+      end
+    end
+  end
+
 end
