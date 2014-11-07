541fd1328cb941fe190c64455b37350f040347c4
Colin Jackson <colin.s.jackson90@gmail.com>
2014-11-07 06:04:13 -0500

    Show or hide notification badge and dropdown appropriately with differing numbers of notifications

--- app/assets/javascripts/views/constants/notification_display.js
+++ app/assets/javascripts/views/constants/notification_display.js
@@ -3,6 +3,8 @@ Dorkle.Views.NotificationDisplay = Backbone.Superview.extend({
   initialize: function () {
     this.$('.notification-dropdown').empty();
     this.$badge = this.$('.notifications-badge');
+    this.$dropdown = this.$('.notification-dropdown');
+    this.updateEls();
 
     this.listenTo(this.collection, 'add', this.addNotification);
     this.listenTo(this.collection, 'remove', this.removeNotification);
@@ -16,25 +18,33 @@ Dorkle.Views.NotificationDisplay = Backbone.Superview.extend({
   },
 
   addNotification: function (notification) {
-    this.$badge.text(this.collection.count);
-
     var subview = new Dorkle.Views.NotificationItem({
       model: notification
     });
 
     this.addSubview('ul.notification-dropdown', subview);
-    this.$badge.text(this.collection.length);
+    this.updateEls();
   },
 
   removeNotification: function (notification) {
-    this.$badge.text(this.collection.length);
-
     var removingSubview;
     _(this.subviews('ul.notification-dropdown')).each(function (subview) {
       if (subview.model === notification) removingSubview = subview;
     });
 
     this.removeSubview('ul.notification-dropdown', removingSubview);
+    this.updateEls();
+  },
+
+  updateEls: function () {
+    this.$badge.text(this.collection.length);
+    if (this.collection.length) {
+      this.$badge.removeClass('is-empty');
+      this.$dropdown.removeClass('is-empty');
+    } else {
+      this.$badge.addClass('is-empty');
+      this.$dropdown.addClass('is-empty');
+    }
   }
 
 });
