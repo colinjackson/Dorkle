faa39ab014ee074ecba6bb7cce8cb1d26c262645
Colin Jackson <colin.s.jackson90@gmail.com>
2014-11-09 23:52:36 -0500

    Style root page leaderboards

--- app/controllers/api/static_pages_controller.rb
+++ app/controllers/api/static_pages_controller.rb
@@ -2,7 +2,7 @@ module Api
   class StaticPagesController < ApplicationController
 
     def root
-      @hot_games = Game.order(rounds_count: :desc).first(5)
+      @hot_games = Game.order(rounds_count: :desc).first(6)
       @best_players = User.order(completed_answer_matches_count: :desc).first(3)
       @best_authors =
         User.order(created_games_completed_rounds_count: :desc).first(3)
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
 
   def root
-    @hot_games = Game.order(rounds_count: :desc).first(5)
+    @hot_games = Game.order(rounds_count: :desc).first(6)
     @best_players = User.order(completed_answer_matches_count: :desc).first(3)
     @best_authors =
       User.order(created_games_completed_rounds_count: :desc).first(3)
